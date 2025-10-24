c6d0f178a989e47251a5f6457b24dcc380851d4f
João Barros <100239951+kawaru2@users.noreply.github.com>
2025-10-24 18:52:34 -0300

    Add files via upload

--- scripts/rolagem.js
+++ scripts/rolagem.js
@@ -2,7 +2,7 @@
     escolhas.forEach(tags=> {
     tags.addEventListener("click", (e) => {
         e.preventDefault()
-        
+
         const n = tags.textContent.toLowerCase()
         const a = document.querySelector(`#${n}`)
         a.scrollIntoView({behavior: "smooth"})
